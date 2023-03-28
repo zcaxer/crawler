b7719df41157501f76933ea60651acbfcaaf8a33
zcaxer <zcaxer@gmail.com>
2023-03-28 23:17:47 +0800

    fix

--- nga/nga/mongo.py
+++ nga/nga/mongo.py
@@ -1,17 +1,23 @@
+# Nga_pymongo module
+
+"""connect mongodb with motor and pymongo"""
+
 #import asyncio
+from typing import Optional
 import motor.motor_asyncio
 import pymongo
 
 
 class Mongo:
-    _instance = None
+    _instance=None
+    async_client: Optional[motor.motor_asyncio.AsyncIOMotorClient] = None
+    sync_client: Optional[pymongo.MongoClient] = None
 
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            cls._instance.async_client = motor.motor_asyncio.AsyncIOMotorClient()
-            cls._instance.sync_client = pymongo.MongoClient(
-                host='localhost', port=27017)
+            cls.async_client = motor.motor_asyncio.AsyncIOMotorClient()
+            cls.sync_client = pymongo.MongoClient(host='localhost', port=27017)
         return cls._instance
 
     async def write_to_db(self, json_data):
--- nga/tests/test_motor.py
+++ nga/tests/test_motor.py
@@ -1,34 +1,19 @@
-# Nga_pymongo module
-
-"""connect mongodb with motor and pymongo"""
-
-#import asyncio
-from typing import Optional
-import motor.motor_asyncio
-import pymongo
-
-class Mongo:
-    _instance: Optional[Mongo] = None
-    async_client: Optional[motor.motor_asyncio.AsyncIOMotorClient] = None
-    sync_client: Optional[pymongo.MongoClient] = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls.async_client = motor.motor_asyncio.AsyncIOMotorClient()
-            cls.sync_client = pymongo.MongoClient(host='localhost', port=27017)
-        return cls._instance
+import unittest
+import sys
+sys.path.append('..')
+from nga.mongo import Mongo
 
 
+class TestMongo(unittest.TestCase):
+    def setUp(self):
+        self.mongo = Mongo()
 
-    async def write_to_db(self,json_data):
-        bson_data=json_data
-        await self.async_client.replies.insert_one(bson_data)
+    def test_read_cookies(self):
+        cookies = {'key': 'value'}
+        self.mongo.write_cookies_to_db(cookies)
+        result = self.mongo.read_cookies()
+        self.assertEqual(result, cookies)
 
-    async def write_cookies_to_db(self,cookies):
-        await self.async_client.nga.info.insert_one({'cookies':cookies})
 
-    def read_cookies(self):
-        
-        cookie=self.sync_client.nga.info.find_one({'cookies':{'$exists':True}})
-        return cookie['cookies']
+if __name__ == '__main__':
+    unittest.main()
